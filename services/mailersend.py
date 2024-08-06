d3f9331207e150d2a5af2825ba00fa0ff48231db
Elijah Karori Orito <elijakarori23@gmail.com>
2024-08-06 16:25:18 +0300

    Update mailersend.py

--- services/mailersend.py
+++ services/mailersend.py
@@ -1,28 +1,38 @@
-import os
 import smtplib
-from dotenv import load_dotenv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+import dotenv
+import string, random
 
-load_dotenv()
+dotenv.load_dotenv()
 
-# Function to send email using smtplib
-def send_email(recipient_email, subject, body):
-    sender_email = os.environ.get("sender")
-    smtp_email = os.environ.get("SMTP_USERNAME")
+def generate_otp(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def send_email(receiver_email, subject, body):
+    sender_email = "customercare@mejack.xyz"
+    username = os.environ.get("SMTP_USERNAME")
     password = os.environ.get("SMTP_PASSWORD")
-    HOST = os.environ.get("SMTP_SERVER")
-    PORT = int(os.environ.get("Port"))
+    stmp_server=os.environ.get("SMTP_SERVER")
 
-    message = f"""\
-Subject: {subject}
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
 
-From: {sender_email}
-
-{body}"""
+    msg.attach(MIMEText(body, 'plain'))
 
     try:
-        with smtplib.SMTP(HOST, PORT) as server:
-            server.login(smtp_email, password)
-            server.sendmail(sender_email, recipient_email, message)
-        print("Email sent successfully.")
+        with smtplib.SMTP(stmp_server, 587) as server:
+            server.starttls()
+            server.login(username, password)
+            server.send_message(msg)
+            print("Email sent successfully")
     except Exception as e:
-        print(f"An error occurred while sending email: {e}")
+        print(f"Error: {e}")
+
+# Example usage
+
+send_email("elijakarori23@gmail.com","test","test message")
